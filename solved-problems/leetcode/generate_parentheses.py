4e8fed8989b6d97c713c54f3831f2297f06ea5b7
c325018 <c325018>
2023-11-16 04:37:11 +0100

    added generate parentheses problem

---
+++ solved-problems/leetcode/generate_parentheses.py
@@ -0,0 +1,46 @@
+class Solution:
+    
+    def backtracking(self, cur_string, left_count, right_count, n, answer):
+
+        # if the string is completed
+        if len(cur_string) == 2*n :
+            answer.append("".join(cur_string))
+            return
+        # left parenthesis can still be added
+        if left_count < n:
+            cur_string.append('(')
+            self.backtracking(cur_string, left_count + 1, right_count, n, answer)
+            cur_string.pop()
+        # right parenthesis can be added to math a previous unmatched left parenthesis
+        if left_count > right_count:
+            cur_string.append(')')
+            self.backtracking(cur_string, left_count, right_count + 1, n, answer)
+            cur_string.pop()
+    
+    def keep_candidate_valid(self, n):
+        answer = []
+        cur_string = []
+        left_count = 0
+        right_count = 0
+        self.backtracking(cur_string, left_count, right_count, n, answer)
+        return answer
+        
+        
+    def generateParenthesis(self, n: int) -> list[str]:
+        
+        # Approach 1: Backtracking considering only valid string candidate
+        return self.keep_candidate_valid(n)
+        
+        
+if __name__ == "__main__":
+    SOL = Solution()
+    n = 3
+    res = SOL.generateParenthesis(n)
+    assert res == ["((()))","(()())","(())()","()(())","()()()"]
+    n = 1
+    res = SOL.generateParenthesis(n)
+    assert res == ["()"]
+
+
+            
+        
