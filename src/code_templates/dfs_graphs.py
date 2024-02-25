79a82c13f3f550655464b586de31dbd61295ff0d
c325018 <florenciopaucar@gmail.com>
2024-02-25 14:55:35 +0100

    reorganized code and added code templates

---
+++ src/code_templates/dfs_graphs.py
@@ -0,0 +1,30 @@
+# Graph: DFS (recursive)
+def fn(graph):
+    def dfs(node):
+        ans = 0
+        # do some logic
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                ans += dfs(neighbor)
+        
+        return ans
+
+    seen = {START_NODE}
+    return dfs(START_NODE)
+
+# Graph: DFS (iterative)
+def fn(graph):
+    stack = [START_NODE]
+    seen = {START_NODE}
+    ans = 0
+
+    while stack:
+        node = stack.pop()
+        # do some logic
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                stack.append(neighbor)
+    
+    return ans
